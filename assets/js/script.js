4edb5bc4166a8e95cf9afa58f8cab13c11b2bea5
Kathryn Fielding <kathrynfielding@Kathryns-MacBook-Air.local>
2023-03-23 11:37:53 -0400

    blog updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,30 +64,43 @@ blog1checkboxEl.addEventListener('change', function() {
         blog4checkboxEl.checked = false;
         blog5checkboxEl.checked = false;
         blog6checkboxEl.checked = false;
+        blog1.style.width = "96%";
+        blog2.style.display = "none";
+        blog3.style.display = "none";
+        blog4.style.display = "none";
+        blog5.style.display = "none";
+        blog6.style.display = "none";
+    } else {
+        blog1.style.width = "46%";
+        blog2.style.display = "block";
+        blog3.style.display = "block";
+        blog4.style.display = "block";
+        blog5.style.display = "block";
+        blog6.style.display = "block";
     }
 });
 blog2checkboxEl.addEventListener('change', function() {
     if(this.checked) {
         console.log("checked");
-        blog1.style.order = "2";
-        blog2.style.order = "1";
-        blog3.style.order = "3";
-        blog4.style.order = "4";
-        blog5.style.order = "5";
-        blog6.style.order = "6";
         blog1checkboxEl.checked = false;
         blog3checkboxEl.checked = false;
         blog4checkboxEl.checked = false;
         blog5checkboxEl.checked = false;
         blog6checkboxEl.checked = false;
+        blog1.style.display = "none";
+        blog2.style.width = "96%";
+        blog3.style.display = "none";
+        blog4.style.display = "none";
+        blog5.style.display = "none";
+        blog6.style.display = "none";
     } else {
         console.log("unchecked");
-        blog1.style.order = "1";
-        blog2.style.order = "2";
-        blog3.style.order = "3";
-        blog4.style.order = "4";
-        blog5.style.order = "5";
-        blog6.style.order = "6";
+        blog1.style.display = "block";
+        blog2.style.width = "46%";
+        blog3.style.display = "block";
+        blog4.style.display = "block";
+        blog5.style.display = "block";
+        blog6.style.display = "block";
     }
 });
 blog3checkboxEl.addEventListener('change', function() {
@@ -98,30 +111,43 @@ blog3checkboxEl.addEventListener('change', function() {
         blog4checkboxEl.checked = false;
         blog5checkboxEl.checked = false;
         blog6checkboxEl.checked = false;
+        blog1.style.display = "none";
+        blog2.style.display = "none";
+        blog3.style.width = "96%";
+        blog4.style.display = "none";
+        blog5.style.display = "none";
+        blog6.style.display = "none";
+    } else {
+        blog1.style.display = "block";
+        blog2.style.display = "block";
+        blog3.style.width = "46%";
+        blog4.style.display = "block";
+        blog5.style.display = "block";
+        blog6.style.display = "block";
     }
 });
 blog4checkboxEl.addEventListener('change', function() {
     if(this.checked) {
         console.log("checked");
-        blog1.style.order = "1";
-        blog2.style.order = "2";
-        blog3.style.order = "4";
-        blog4.style.order = "3";
-        blog5.style.order = "5";
-        blog6.style.order = "6";
         blog1checkboxEl.checked = false;
         blog2checkboxEl.checked = false;
         blog3checkboxEl.checked = false;
         blog5checkboxEl.checked = false;
         blog6checkboxEl.checked = false;
+        blog1.style.display = "none";
+        blog2.style.display = "none";
+        blog3.style.display = "none";
+        blog4.style.width = "96%";
+        blog5.style.display = "none";
+        blog6.style.display = "none";
     } else {
         console.log("unchecked");
-        blog1.style.order = "1";
-        blog2.style.order = "2";
-        blog3.style.order = "3";
-        blog4.style.order = "4";
-        blog5.style.order = "5";
-        blog6.style.order = "6";
+        blog1.style.display = "block";
+        blog2.style.display = "block";
+        blog3.style.display = "block";
+        blog4.style.width = "46%";
+        blog5.style.display = "block";
+        blog6.style.display = "block";
     }
 });
 blog5checkboxEl.addEventListener('change', function() {
@@ -132,29 +158,42 @@ blog5checkboxEl.addEventListener('change', function() {
         blog3checkboxEl.checked = false;
         blog4checkboxEl.checked = false;
         blog6checkboxEl.checked = false;
+        blog1.style.display = "none";
+        blog2.style.display = "none";
+        blog3.style.display = "none";
+        blog4.style.display = "none";
+        blog5.style.width = "96%";
+        blog6.style.display = "none";
+    } else {
+        blog1.style.display = "block";
+        blog2.style.display = "block";
+        blog3.style.display = "block";
+        blog4.style.display = "block";
+        blog5.style.width = "46%";
+        blog6.style.display = "block";
     }
 });
 blog6checkboxEl.addEventListener('change', function() {
     if(this.checked) {
         console.log("checked");
-        blog1.style.order = "1";
-        blog2.style.order = "2";
-        blog3.style.order = "3";
-        blog4.style.order = "4";
-        blog5.style.order = "6";
-        blog6.style.order = "5";
         blog1checkboxEl.checked = false;
         blog2checkboxEl.checked = false;
         blog3checkboxEl.checked = false;
         blog4checkboxEl.checked = false;
         blog5checkboxEl.checked = false;
+        blog1.style.display = "none";
+        blog2.style.display = "none";
+        blog3.style.display = "none";
+        blog4.style.display = "none";
+        blog5.style.display = "none";
+        blog6.style.width = "96%";
     } else {
         console.log("unchecked");
-        blog1.style.order = "1";
-        blog2.style.order = "2";
-        blog3.style.order = "3";
-        blog4.style.order = "4";
-        blog5.style.order = "5";
-        blog6.style.order = "6";
+        blog1.style.display = "block";
+        blog2.style.display = "block";
+        blog3.style.display = "block";
+        blog4.style.display = "block";
+        blog5.style.display = "block";
+        blog6.style.width = "46%";
     }
 });
