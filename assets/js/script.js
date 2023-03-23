ced5cb8970cfb7b49bf9b86605efc22de082148a
Kathryn Fielding <kathrynfielding@Kathryns-MacBook-Air.local>
2023-03-22 23:48:42 -0400

    first draft of blog template

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,122 @@ contactUsButtonEl.forEach(contactForm => {
 //         console.log(message);
 //     })
 // })
+
+//blog checkboxes
+var blog1checkboxEl = document.querySelector("#blog1-toggle");
+var blog2checkboxEl = document.querySelector("#blog2-toggle");
+var blog3checkboxEl = document.querySelector("#blog3-toggle");
+var blog4checkboxEl = document.querySelector("#blog4-toggle");
+var blog5checkboxEl = document.querySelector("#blog5-toggle");
+var blog6checkboxEl = document.querySelector("#blog6-toggle");
+
+//blog entries
+var blog1 = document.querySelector("#blog1");
+var blog2 = document.querySelector("#blog2");
+var blog3 = document.querySelector("#blog3");
+var blog4 = document.querySelector("#blog4");
+var blog5 = document.querySelector("#blog5");
+var blog6 = document.querySelector("#blog6");
+
+blog1checkboxEl.addEventListener('change', function() {
+    if(this.checked) {
+        console.log("checked");
+        blog2checkboxEl.checked = false;
+        blog3checkboxEl.checked = false;
+        blog4checkboxEl.checked = false;
+        blog5checkboxEl.checked = false;
+        blog6checkboxEl.checked = false;
+    }
+});
+blog2checkboxEl.addEventListener('change', function() {
+    if(this.checked) {
+        console.log("checked");
+        blog1.style.order = "2";
+        blog2.style.order = "1";
+        blog3.style.order = "3";
+        blog4.style.order = "4";
+        blog5.style.order = "5";
+        blog6.style.order = "6";
+        blog1checkboxEl.checked = false;
+        blog3checkboxEl.checked = false;
+        blog4checkboxEl.checked = false;
+        blog5checkboxEl.checked = false;
+        blog6checkboxEl.checked = false;
+    } else {
+        console.log("unchecked");
+        blog1.style.order = "1";
+        blog2.style.order = "2";
+        blog3.style.order = "3";
+        blog4.style.order = "4";
+        blog5.style.order = "5";
+        blog6.style.order = "6";
+    }
+});
+blog3checkboxEl.addEventListener('change', function() {
+    if(this.checked) {
+        console.log("checked");
+        blog1checkboxEl.checked = false;
+        blog2checkboxEl.checked = false;
+        blog4checkboxEl.checked = false;
+        blog5checkboxEl.checked = false;
+        blog6checkboxEl.checked = false;
+    }
+});
+blog4checkboxEl.addEventListener('change', function() {
+    if(this.checked) {
+        console.log("checked");
+        blog1.style.order = "1";
+        blog2.style.order = "2";
+        blog3.style.order = "4";
+        blog4.style.order = "3";
+        blog5.style.order = "5";
+        blog6.style.order = "6";
+        blog1checkboxEl.checked = false;
+        blog2checkboxEl.checked = false;
+        blog3checkboxEl.checked = false;
+        blog5checkboxEl.checked = false;
+        blog6checkboxEl.checked = false;
+    } else {
+        console.log("unchecked");
+        blog1.style.order = "1";
+        blog2.style.order = "2";
+        blog3.style.order = "3";
+        blog4.style.order = "4";
+        blog5.style.order = "5";
+        blog6.style.order = "6";
+    }
+});
+blog5checkboxEl.addEventListener('change', function() {
+    if(this.checked) {
+        console.log("checked");
+        blog1checkboxEl.checked = false;
+        blog2checkboxEl.checked = false;
+        blog3checkboxEl.checked = false;
+        blog4checkboxEl.checked = false;
+        blog6checkboxEl.checked = false;
+    }
+});
+blog6checkboxEl.addEventListener('change', function() {
+    if(this.checked) {
+        console.log("checked");
+        blog1.style.order = "1";
+        blog2.style.order = "2";
+        blog3.style.order = "3";
+        blog4.style.order = "4";
+        blog5.style.order = "6";
+        blog6.style.order = "5";
+        blog1checkboxEl.checked = false;
+        blog2checkboxEl.checked = false;
+        blog3checkboxEl.checked = false;
+        blog4checkboxEl.checked = false;
+        blog5checkboxEl.checked = false;
+    } else {
+        console.log("unchecked");
+        blog1.style.order = "1";
+        blog2.style.order = "2";
+        blog3.style.order = "3";
+        blog4.style.order = "4";
+        blog5.style.order = "5";
+        blog6.style.order = "6";
+    }
+});
