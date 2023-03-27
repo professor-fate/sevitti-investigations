21690a1748cbd1460bbdc762b03cc0c03fa25c75
eric Smith <ericandrewsmith88@gmail.com>
2023-03-27 15:39:55 -0400

    intake form close button bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,11 +24,22 @@ contactUsButtonEl.forEach(contactForm => {
         navEl.style.filter="blur(10px)";
         mainEl.style.filter="blur(10px)";
         footerEl.style.filter="blur(10px)";
+
+        headerEl.style['pointer-events'] = 'none';
+        navEl.style['pointer-events'] = 'none';
+        mainEl.style['pointer-events'] = 'none';
+        footerEl.style['pointer-events'] = 'none';
+
+        headerEl.style['user-select'] = 'none';
+        navEl.style['user-select'] = 'none';
+        mainEl.style['user-select'] = 'none';
+        footerEl.style['user-select'] = 'none';
     })
 })
 
 //close intake form
 closeFormButtonEl.addEventListener('click', function handleClick(event) {
+    event.preventDefault();
     intakeFormEl.style.display = "none";
     headerEl.classList.toggle('inactive');
     navEl.classList.toggle('inactive');
@@ -38,6 +49,16 @@ closeFormButtonEl.addEventListener('click', function handleClick(event) {
     navEl.style.filter="blur(0)";
     mainEl.style.filter="blur(0)";
     footerEl.style.filter="blur(0)";
+
+    headerEl.style['pointer-events'] = 'auto';
+        navEl.style['pointer-events'] = 'auto';
+        mainEl.style['pointer-events'] = 'auto';
+        footerEl.style['pointer-events'] = 'auto';
+
+        headerEl.style['user-select'] = 'auto';
+        navEl.style['user-select'] = 'auto';
+        mainEl.style['user-select'] = 'auto';
+        footerEl.style['user-select'] = 'auto';
 })
 
 //popup submit
