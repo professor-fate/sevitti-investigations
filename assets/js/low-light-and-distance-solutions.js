ada78f62d49ad0d6e65fb6fe8c63ea4100408e1c
eric Smith <ericandrewsmith88@gmail.com>
2023-04-22 15:05:44 -0400

    zoom in out feature added

--- assets/js/low-light-and-distance-solutions.js
+++ assets/js/low-light-and-distance-solutions.js
@@ -1,4 +1,17 @@
 //distance image switch
+const image = document.getElementById('zoom-in-out');
+
+let toggle = true;
+image.addEventListener('click', function() {
+    toggle = !toggle;
+    if(toggle){
+        image.style.transform = "scale(1)";
+        setTimeout(() => image.src = "./assets/images/capability-images/llandds-top.png", 1100);
+    } else {
+        image.style.transform = "scale(4)";
+        setTimeout(() => image.src = "./assets/images/capability-images/llandds-bottom.png", 1100);
+    }
+})
 
 //low light image slider
 var container = document.querySelector('.before-after-container');
