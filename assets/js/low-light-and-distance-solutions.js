7d9716f2f788bccaaf8f8bc21cbfeac8dde3c91b
eric Smith <ericandrewsmith88@gmail.com>
2023-05-20 14:16:43 -0400

    zoom in out function turned off

--- assets/js/low-light-and-distance-solutions.js
+++ assets/js/low-light-and-distance-solutions.js
@@ -1,21 +1,20 @@
-//distance image switch
-const image = document.getElementById('zoom-in-out');
+// //distance image switch
+// const image = document.getElementById('zoom-in-out');
 
-let toggle = true;
-image.addEventListener('click', function() {
-    toggle = !toggle;
-    if(toggle){
-        image.style.transform = "scale(1)";
-        image.src = "./assets/images/capability-images/llandds-top.webp";
-    } else {
-        image.style.transition = "2s";
-        image.style.transform = "scale(4)";
-        setTimeout(() => image.src = "./assets/images/capability-images/llandds-bottom.webp", 1000);
-        setTimeout(() => image.style.transition = "0s", 1000);
-        setTimeout(() => image.style.transform = "scale(1)", 1000);
-        
-    }
-})
+// let toggle = true;
+// image.addEventListener('click', function() {
+//     toggle = !toggle;
+//     if(toggle){
+//         image.style.transform = "scale(1)";
+//         image.src = "./assets/images/capability-images/llandds-top.webp";
+//     } else {
+//         image.style.transition = "2s";
+//         image.style.transform = "scale(4)";
+//         setTimeout(() => image.src = "./assets/images/capability-images/llandds-bottom.webp", 1000);
+//         setTimeout(() => image.style.transition = "0s", 1000);
+//         setTimeout(() => image.style.transform = "scale(1)", 1000);
+//     }
+// })
 
 //low light image slider
 var container = document.querySelector('.before-after-container');
