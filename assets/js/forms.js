b8f8fe59a347b7ea74b050e0728d4fc13bfa95b2
eric Smith <ericandrewsmith88@gmail.com>
2023-05-31 22:04:17 -0400

    customer forms formatted for intake forms

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -1,7 +1,36 @@
+// print button
 printButtonEl = document.querySelectorAll('.printbtn');
 
 printButtonEl.forEach(printButton => {
     printButton.addEventListener("click", function handleClick (event) {
         print();
     })
+})
+
+//display form
+var headerEl = document.querySelector("#header");
+var navEl = document.querySelector("#nav");
+var mainEl = document.querySelector("#main");
+var footerEl = document.querySelector("#footer");
+
+requestNewButtonEl = document.querySelector('#ir-new');
+requestNewFormEl = document.querySelector('#request-new')
+
+requestNewButtonEl.addEventListener("click", function handleClick (event) {
+    requestNewFormEl.style.display = "block";
+    headerEl.style.display="none";
+    navEl.style.display="none";
+    mainEl.style.display="none";
+    footerEl.style.display="none";
+})
+
+requestResearchButtonEl = document.querySelector('#ir-research');
+requestResearchFormEl = document.querySelector('#request-research')
+
+requestResearchButtonEl.addEventListener("click", function handleClick (event) {
+    requestResearchFormEl.style.display = "block";
+    headerEl.style.display="none";
+    navEl.style.display="none";
+    mainEl.style.display="none";
+    footerEl.style.display="none";
 })
