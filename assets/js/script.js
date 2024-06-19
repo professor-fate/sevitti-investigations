d507fe61b3f946adff2111d39be661dcd1265259
eric Smith <ericandrewsmith88@gmail.com>
2024-06-18 20:45:43 -0400

    meta description tags added

--- assets/js/script.js
+++ assets/js/script.js
@@ -217,3 +217,36 @@ heroImage.onload = function() {
     //         },
     //     });
     // };
+
+
+
+    //turnstile code
+
+    // This is the demo secret key. In production, we recommend
+
+// const SECRET_KEY = $process.env.TURNSTILE_SECRET_KEY
+
+// async function handlePost(request) {
+// 	const body = await request.formData();
+// 	// Turnstile injects a token in "cf-turnstile-response".
+// 	const token = body.get('cf-turnstile-response');
+// 	const ip = request.headers.get('CF-Connecting-IP');
+
+// 	// Validate the token by calling the
+// 	// "/siteverify" API endpoint.
+// 	let formData = new FormData();
+// 	formData.append('secret', SECRET_KEY);
+// 	formData.append('response', token);
+// 	formData.append('remoteip', ip);
+
+// 	const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+// 	const result = await fetch(url, {
+// 		body: formData,
+// 		method: 'POST',
+// 	});
+
+// 	const outcome = await result.json();
+// 	if (outcome.success) {
+// 		console.log("it works!");
+// 	}
+// }
